2180c1d9079983fbce1405c65c90dc1b0dfd4ce4
Emily <emiyxlin@gmail.com>
2025-04-04 17:52:52 +0800

    Initial commit from old project

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    //fullpage plugin initialization
+    $('#fullpage').fullpage({
+        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
+        menu: '#js-menu',
+        autoScrolling: false,
+        fitToSection: false,
+        ScrollingSpeed: 1200
+    });
+
+    //logo change when scroll
+    $(window).on('scroll',function(){
+        var scrollDistance = $(window).scrollTop();
+        var $header = $(".js-header");
+        if(scrollDistance > 80){
+            $header.addClass("header--scrolling");
+        }
+        else{
+            $header.removeClass("header--scrolling")
+        }
+    })
+
+    //rellax initialization
+    if (screen.width > 768) {
+        var rellax = new Rellax('.rellax',{
+            center: true
+        });
+    }
+
+    //owl-carousel initialization
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:16,
+        nav:true,
+        navText:[],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            1600:{
+                items:4
+            } 
+        }
+    });
+});
